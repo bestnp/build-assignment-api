80f18ba0446cda7db0bbbfe0b77f903a5cf286a0
Jue Parinthon <jue.prt@hotmail.com>
2025-09-01 22:46:09 +0700

    feat: build creating data API

--- server/app.mjs
+++ server/app.mjs
@@ -1,11 +1,45 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
 
-app.get("/test", (req, res) => {
-  return res.json("Server API is working 🚀");
-});
+app.use(express.json());
+
+app.post("/assignments", async (req, res) => {
+  const newAssignment = {
+    ...req.body,
+    created_at: new Date(),
+    updated_at: new Date(),
+    published_at: new Date(),
+  };
+
+  try {
+     await connectionPool.query(
+      `INSERT INTO assignments (user_id ,title, content, category, length, status, created_at, updated_at, published_at)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+      [
+        newAssignment.user_id,
+        newAssignment.title,
+        newAssignment.content,
+        newAssignment.category,
+        newAssignment.length,
+        newAssignment.status,
+        newAssignment.created_at,
+        newAssignment.updated_at,
+        newAssignment.published_at,
+      ]
+    );
+  } catch (error) {
+    return res.status(500).json({
+       "message": "Server could not create assignment because database connection" 
+      })
+  }
+
+  return res.status(201).json({
+    "message": "Create assignment successfully",
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
